e65e047137c39a191aedeaf5e5a070f8b9e28cd8
nimranaz148 <159953826+nimranaz148@users.noreply.github.com>
2026-04-01 19:04:55 +0500

    routes-complte

--- src/routes/ai.routes.js
+++ src/routes/ai.routes.js
@@ -1,7 +1,7 @@
 import express from "express"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 import { requireProjectMember } from "../middlewares/project.middleware.js"
-import {suggestTasks, analyzeRisks} from "../controllers/ai.controllers.js"
+import {suggestTasks, analyzeRisks, predictTimeline, balanceWorkload, smartassignTask, prioritizeTask, summarizeMeeting} from "../controllers/ai.controllers.js"
 
 const router = express.Router()
 
@@ -10,5 +10,16 @@ router.use(verifyJWT)
 router.route("/suggest-tasks/:projectId").all(requireProjectMember).post(suggestTasks)
 
 router.route("/analyze-risks/:projectId").all(requireProjectMember).get(analyzeRisks)
+
+router.route("/predict-timeline/:projectId").all(requireProjectMember).get(predictTimeline)
+
+router.route("/balance-workload/:projectId").all(requireProjectMember).get(balanceWorkload)
+
+// localhost:8000/api/ai/xxxx-xx/:taskId
+router.route("/assign-task/:taskId").post(smartassignTask)
+router.route("/prioritize/:taskId").post(prioritizeTask)
+
+// general AI feature there are not project specific, just task specific
+router.route("/summarize-meeting").post(summarizeMeeting)
 
 export default router
